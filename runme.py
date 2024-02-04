2a5d6e1762f6422c8c9aad3cc3f21b75ae3a0951
seanmain <seanmain@btinternet.com>
2024-02-04 14:28:47 +0000

    Updated py

--- runme.py
+++ runme.py
@@ -1,9 +1,27 @@
 import os
 import sys
+import requests
+from beaupy.spinners import Spinner, DOTS
+from beaupy import confirm, select, select_multiple
 
 
 def main():
-    print("Python is working")
+    args = sys.argv()
+    try:
+        token = args[1]
+    except:
+        print('You must provide a token')
+        sys.exit(-1)
+    
+    spinner = Spinner(DOTS, "Testing download...")
+    spinner.start()
+    url = ""
+    temp_headers = {
+        "X-Auth-Token": token
+    }
+    r = requests.get(url, headers=temp_headers)
+    spinner.stop()
+    print(f"Download completed with code {r.status_code}")
 
 
 if __name__ in ['__main__', 'builtin', '__buitlins__']:
