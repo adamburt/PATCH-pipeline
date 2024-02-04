005815777f517defccc754b8641ae816dcfa8fa0
seanmain <seanmain@btinternet.com>
2024-02-04 14:32:01 +0000

    Udated

--- runme.py
+++ runme.py
@@ -3,15 +3,24 @@ import sys
 import requests
 from beaupy.spinners import Spinner, DOTS
 from beaupy import confirm, select, select_multiple
+import argparse
+
+
+def setup_args():
+    args = argparse.ArgumentParser()
+
+    args.add_argument('-u', '--url', action='store', help='URL to download')
+    args.add_argument('-t', '--token', action='store', help='Token to token')
+
+    parser = args.parse_args()
+
+    return parser
 
 
 def main():
-    args = sys.argv
-    try:
-        token = args[1]
-    except:
-        print('You must provide a token')
-        sys.exit(-1)
+    args = setup_args()
+    url = args.url
+    token = args.token
     
     spinner = Spinner(DOTS, "Testing download...")
     spinner.start()
